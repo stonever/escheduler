c607001ed7d2e7eecb49dc85ce752b711308cabb
江磊 <lei@jiangleis-MacBook-Pro.local>
2023-01-27 21:23:04 +0800

    fix log

---
+++ assigner_test.go
@@ -0,0 +1,21 @@
+package escheduler
+
+import (
+	"go.etcd.io/etcd/api/v3/mvccpb"
+	"testing"
+)
+
+func TestAssigner_GetReBalanceResult(t *testing.T) {
+	var a Assigner
+	workerList := make([]string, 0)
+	taskPathResp := make([]*mvccpb.KeyValue, 0)
+	taskMap := make(map[string]Task)
+	gotToDeleteWorkerTaskKey, gotToDeleteTaskKey, gotAssignMap, err := a.GetReBalanceResult(workerList, taskMap, taskPathResp)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(gotToDeleteWorkerTaskKey)
+	t.Log(gotToDeleteTaskKey)
+	t.Log(gotAssignMap)
+
+}
